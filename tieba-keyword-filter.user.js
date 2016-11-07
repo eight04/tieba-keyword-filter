97667877dc8691d2a36df1aae01a6175d57e6ed3
eight <eight04@gmail.com>
2016-11-07 13:56:17 +0800

    Initial commit

---
+++ tieba-keyword-filter.user.js
@@ -0,0 +1,56 @@
+// ==UserScript==
+// @name        Tieba Keyword Filter
+// @namespace   eight04.blogspot.com
+// @description 依關鍵字摺疊樓層廣告
+// @include     http://tieba.baidu.com/*
+// @version     0.0.0
+// @grant       GM_addStyle
+// @grant		GM_registerMenuCommand
+// @grant		GM_getValue
+// @grant		GM_setValue
+// @require https://greasyfork.org/scripts/7212-gm-config-eight-s-version/code/GM_config%20(eight's%20version).js?version=156587
+// ==/UserScript==
+
+var keywords;
+
+GM_config.setup(
+	{
+		keywords: {
+			label: "關鍵字（一行一個）",
+			type: "textarea",
+			default: "jiaoyimao.com/goods\n广告"
+		}
+	},
+	function(){
+		keywords = GM_config.get("keywords").split("\n");
+	}
+);
+
+GM_addStyle(".tkf-hidden{height: 10px; overflow:hidden;} #j_p_postlist > div:not(.l_post):not(#j_p_postlist){display:none;} #thread_list > li:not(.thread_top_list_folder):not(.j_thread_list){display: none;}");
+
+function toggle(e) {
+	this.classList.toggle("tkf-hidden");
+}
+
+function hide() {
+	var posts = document.querySelectorAll(".l_post");
+
+	for (let post of posts) {
+		for (let keyword of keywords) {
+			if (post.textContent.includes(keyword)) {
+				post.classList.add("tkf-hidden");
+				post.addEventListener("click", toggle);
+				break;
+			}
+		}
+	}
+}
+
+hide();
+
+new MutationObserver(hide).observe(
+	document.querySelector("#j_p_postlist"),
+	{
+		childList: true
+	}
+);
